4b8891763185c7ad17c3170eecac8b77eb6a0f04
Jacob Tepperman <jtepp@icloud.com>
2020-08-11 02:35:01 -0400

    added parsing with regex

--- src/dateParse.js
+++ src/dateParse.js
@@ -1,18 +1,37 @@
 export async function handler(event, context) {
 const url = require('url')
-const s = event.queryStringParameters["a"]
+var s = event.queryStringParameters["a"].toLowerCase()
+// if(s.contains("TBD"))
+// console.log(s)
+if(s.includes("tbd"))
+return {
+  statusCode: 200,
+  body: "TBD"
+}
+//default year to 2020
+if(!s.match(/\d{4}/)) s="2020 "+s //add 2020 if no year found
+
+//deal with NET
+if(s.includes('net')) s=s.replace('net','')
+
+//deal with 6/7
+if(s.match(/\d\/\d/)){
+  let x = s.match(/(?<=\d)\/\d/)
+ s = s.replace(x,'')
+}
+
+//deal with early
+if(s.includes('early')) s=s.replace('early','01 01')
+
+//deal with late
+if(s.includes('late')) s=s.replace('late','sep 20')
+
   try {
-  if(s){
     let r = new Date(s).toDateString()
     return {
       statusCode: 200,
       body: r
-    } }else {
-      let r = new Date(s).toDateString()
-      return {
-      statusCode: 200,
-      body: r
-    }}
+    }
   } catch (e){
       return {
         statusCode: 400,
