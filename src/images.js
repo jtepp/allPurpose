d6467ef8b4122e6b84975a18929bbb491f5bacfe
Jacob Tepperman <jtepp@icloud.com>
2020-08-25 19:34:38 -0400

    Fixed offset, added wraparound

--- src/images.js
+++ src/images.js
@@ -3,17 +3,16 @@ import fetch from "node-fetch";
 exports.handler = async (event, context) => {
 
     const API_ENDPOINT = "https://www.google.com/search?q="+event.queryStringParameters["q"]+"&tbm=isch&safe=active";
-    let slyce = 0
-    let offset = 0
-    if (event.queryStringParameters["offset"]!=undefined)
-    {
+    var slyce = 0
+    var offset = 0
+    if (event.queryStringParameters["offset"]!=undefined) {
         if (event.queryStringParameters["offset"]<0)
         {
             offset = 0
             slyce = -1
         }
         else 
-        offset = Number(event.queryStringParameters["offset"])
+        offset = event.queryStringParameters["offset"]
     }
         
 //         //give img
@@ -29,20 +28,26 @@ exports.handler = async (event, context) => {
 //         })
 //     .catch(error => ({ statusCode: 422, body: String(error) }));
     
-    else if (event.queryStringParameters["url"]!=undefined)//give url
+    if (event.queryStringParameters["url"]!=undefined)//give url
     return fetch(API_ENDPOINT, { headers: {} })
     .then(response => response.text())
-    .then(data => ({
+    .then(data => {
+        offset %= [...data.matchAll(/(?<=<img.+?alt.+?src=").+?(?=")/g)][0].slice(slyce)[offset].length
+        return ({
             statusCode: 200,
-            body: [...data.matchAll(/(?<=<img.+?alt.+?src=").+?(?=")/g)][0].slice(slyce)[offset]})
+            body: [...data.matchAll(/(?<=<img.+?alt.+?src=").+?(?=")/g)][0].slice(slyce)[offset]})}
             )
     .catch(error => ({ statusCode: 422, body: String(error) }));
     
     else if (event.queryStringParameters["b64"]!=undefined)//give b64
     return fetch('https://www.google.com/search?q='+event.queryStringParameters["q"], { headers: {} })
     .then(response => response.text())
-    .then(data => ({
+    .then(data => {
+        
+            offset %= [...data.matchAll(/(?<=data:image\/jpeg\;base64,).+?(?=')/g)].length
+        return ({
             statusCode: 200,
-            body: [...data.matchAll(/(?<=data:image\/jpeg\;base64,).+?(?=')/g)][0].split('\\').join('').slice(slyce)[offset]}))
+            body: [...data.matchAll(/(?<=data:image\/jpeg\;base64,).+?(?=')/g)].slice(slyce)[offset][0].split('\\').join('')})}
+            )
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
