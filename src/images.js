1aa42b47cbdc33237081d5a8c7e09afdcb9f84ad
Jacob Tepperman <jtepp@icloud.com>
2020-08-25 18:45:29 -0400

    &offset parameter added

--- src/images.js
+++ src/images.js
@@ -3,25 +3,39 @@ import fetch from "node-fetch";
 exports.handler = async (event, context) => {
 
     const API_ENDPOINT = "https://www.google.com/search?q="+event.queryStringParameters["q"]+"&tbm=isch&safe=active";
-
-        //give img
-    if (event.queryStringParameters["img"]!=undefined)
-  return fetch(API_ENDPOINT, { headers: {} })
-    .then(response => response.text())
-    .then(data => {
-        return fetch(data.match(/(?<=<img.+?alt.+?src=").+?(?=")/)[0])
-        .then(res => res.text())
-        .then(data => ({
-            statusCode: 200,
-            body: data}))
-        })
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+    let slyce = 0
+    let offset = 0
+    if (event.queryStringParameters["offset"]!=undefined)
+    {
+        if (event.queryStringParameters["offset"]<0)
+        {
+            offset = 0
+            slyce = -1
+        }
+        else 
+        offset = Number(offset)
+    }
+        
+//         //give img
+//     if (event.queryStringParameters["img"]!=undefined)
+//   return fetch(API_ENDPOINT, { headers: {} })
+//     .then(response => response.text())
+//     .then(data => {
+//         return fetch(data.match(/(?<=<img.+?alt.+?src=").+?(?=")/g)[0])
+//         .then(res => res.text())
+//         .then(data => ({
+//             statusCode: 200,
+//             body: data}))
+//         })
+//     .catch(error => ({ statusCode: 422, body: String(error) }));
     
     else if (event.queryStringParameters["url"]!=undefined)//give url
     return fetch(API_ENDPOINT, { headers: {} })
     .then(response => response.text())
-    .then(data => {
-        return data.match(/(?<=<img.+?alt.+?src=").+?(?=")/)[0]})
+    .then(data => ({
+            statusCode: 200,
+            body: [...data.matchAll(/(?<=<img.+?alt.+?src=").+?(?=")/g)].slice(slyce)[offset]})
+            )
     .catch(error => ({ statusCode: 422, body: String(error) }));
     
     else if (event.queryStringParameters["b64"]!=undefined)//give b64
@@ -29,6 +43,6 @@ exports.handler = async (event, context) => {
     .then(response => response.text())
     .then(data => ({
             statusCode: 200,
-            body: data.match(/(?<=data:image\/jpeg\;base64,).+?(?=')/)[0].split('\\').join('')}))
+            body: [...data.matchAll(/(?<=data:image\/jpeg\;base64,).+?(?=')/g)][offset].split('\\').join('').slice(slyce)[offset]}))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
