69d46eb7095ba450bd4b96b2a3b0809da447cea2
Jacob Tepperman <jtepp@icloud.com>
2020-08-26 23:09:49 -0400

    .json() lowercase

--- src/coordinates.js
+++ src/coordinates.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
     const API_ENDPOINT = "https://api.opencagedata.com/geocode/v1/json?key=9d61707dc7d9479491196d53aba7251f&q="+event.queryStringParameters["a"]+"&pretty=0&no_annotations=1";
     
     return fetch(API_ENDPOINT, { headers: {} })
-    .then(response => response.JSON())
+    .then(response => response.json())
     .then(data => {
         var r = data.results[0].geometry
            
