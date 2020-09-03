cb7dfd40100f63adc4fafbf31eefc68de70fa2ed
Jacob Tepperman <jtepp@icloud.com>
2020-09-03 16:02:22 -0400

    added breaks and trying to make imgtoname work better

--- src/missions.js
+++ src/missions.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
             mission = {
                 value1: `${name.join(" - ")}`,
                 value2: `${launchTime} on ${date}`,
-                value3: "https://allpurpose.netlify.app/resources/"+imgName(name[0].toLowerCase())+".png",
+                value3: "https://allpurpose.netlify.app/resources/"+imgName(name.join(" - ").toLowerCase())+".png",
             }
         
             else mission = {
@@ -54,36 +54,37 @@ function imgName(name) {
     switch (name) {
         case name.match(/ariane|vega/):
             return "arianespace-logo"
-            
+            break;
         case name.match(/rocket \d+.\d+/):
             return "astra-logo"
-            
+            break;
         case name.match(/cz-|lm-|shenzou|long march/):
             return "cnsa-logo"
-            
+            break;
         case name.match(/slv/):
             return "isro-logo"
-            
+            break;
         case name.match(/antares|cygnus|pegasus|minotaur|peacekeeper/):
             return "orbital-logo"
-            
+            break;
         case name.match(/electron|photon/):
             return "rocketlab-logo"
-            
+            break;
         case name.match(/soyuz|angara/):
             return "roscosmos-logo"
-            
+            break;
         case name.match(/falcon/):
             return "spacex-logo"
-            
+            break;
         case name.match(/delta|atlas|vulcan|centaur/):
             return "ula-logo"
-            
+            break;
         case name.match(/launcherone/):
             return "virgin-logo"
-            
+            break;
         default:
             return "rocket-icon"
+            break;
         }
 
 }
