737ca2398985884738c269f4499ac02b6e263b0d
Jacob Tepperman <jtepp@icloud.com>
2020-09-07 19:58:29 -0400

    Added reddit endpoint

---
+++ src/reddit.js
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+
+exports.handler = async (event, context) => {
+    
+    const limit = event.queryStringParameters["limit"] || 1
+const sub = event.queryStringParameters["sub"]
+const sort = event.queryStringParameters["sort"]
+    const API_ENDPOINT = "https://reddit.com/r/"+sub+"/"+sort+".json"
+    
+    return fetch(API_ENDPOINT, { headers: {} })
+    .then(response => response.json())
+    .then(data => {
+        var posts = []
+        var r = data["data"]["children"]
+            for (let i = 0; i<limit; i++){
+               const p = r[i]["data"]
+               posts.push(new Post(p.title, p["author"], p.ups, p.selftext, p.url, p.url.includes('.jpg') || p.url.includes('.png') || p.url.includes('.gif')))
+           }
+        return ({
+            statusCode: 200,
+            body: JSON.stringify(posts)
+        })}
+            )
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
+
+class Post {
+    constructor(title, author, ups, text, url, image){
+        this.title = title
+        this.author = author
+        this.ups = ups
+        this.text = text
+        this.url = url
+        this.image = image
+    }
+
+}
