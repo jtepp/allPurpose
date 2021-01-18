9943c9b885e01a17726ff14f321091931abe3c43
Jacob Tepperman <jtepp@icloud.com>
2021-01-17 22:38:20 -0500

    trying again with david wells' code

--- src/netid.js
+++ src/netid.js
@@ -1,30 +1,54 @@
-const chromium = require('chrome-aws-lambda');
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
+  let theTitle = null
+  let browser = null
+  console.log('spawning chrome headless')
+  try {
+    const executablePath = await chromium.executablePath
 
-    const pageToScreenshot = 'https://jacobtepperman.com'
+    // setup
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: executablePath,
+      headless: chromium.headless,
+    })
 
-    const browser = await chromium.puppeteer.launch({
-        executablePath: await chromium.executablePath,
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        headless: chromium.headless,
-    });
-    
-    const page = await browser.newPage();
+    // Do stuff with headless chrome
+    const page = await browser.newPage()
+    const targetUrl = 'https://davidwells.io'
 
-    await page.goto(pageToScreenshot);
+    // Goto page and then do stuff
+    await page.goto(targetUrl, {
+      waitUntil: ["domcontentloaded", "networkidle0"]
+    })
 
-    const screenshot = await page.screenshot({ encoding: 'binary' });
+    await page.waitForSelector('#phenomic')
 
-    await browser.close();
-  
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ 
-            message: `Complete screenshot of ${pageToScreenshot}`, 
-            buffer: screenshot 
-        })
+    theTitle = await page.title();
+
+    console.log('done on page', theTitle)
+
+  } catch (error) {
+    console.log('error', error)
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error
+      })
+    })
+  } finally {
+    // close browser
+    if (browser !== null) {
+      await browser.close()
     }
+  }
 
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      title: theTitle,
+    })
+  })
 }
