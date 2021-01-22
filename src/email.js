7807b31107eef5fdc0a24d16963fc9c3f6c13d46
Jacob Tepperman <jtepp@icloud.com>
2021-01-22 17:48:10 -0500

    added my email

--- src/email.js
+++ src/email.js
@@ -21,7 +21,7 @@ export async function handler(event, context) {
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
+    to: "jtepp@icloud.com, baz@example.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
