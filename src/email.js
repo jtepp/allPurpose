f04e2edad303180864e0726fb36f8a0924f77287
Jacob Tepperman <jtepp@icloud.com>
2021-01-23 16:42:44 -0500

    a working nodemailer

--- src/email.js
+++ src/email.js
@@ -1,51 +1,40 @@
-
-const nodemailer = require("nodemailer");
-
-// async..await is not allowed in global scope, must use a wrapper
 export async function handler(event, context) {
-  try {
-    let transporter = nodemailer.createTransport({
-        service: "hotmail",
-        auth: {
-            user: "group724a@outlook.com",
-            pass: "icons724A"
-        }
-    })
 
-    const options = {
-        from :"group724a@outlook.com",
-        to: "jtepp@icloud.com",
-        subject: "TEST",
-        text: "SDFOIDSJF"
+  const nodemailer = require('nodemailer');
+
+  let recipient = valid(event.queryStringParameters["r"]) || "jtepp@icloud.com"
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'iconsrequestservice@gmail.com',
+      pass: 'icons724A'
     }
-    console.log(true)
+  });
 
-    transporter.sendMail(options, (err, info)=>{
-        if (err) {
-           console.log(err)
-            return
-        }
-        console.log("Sent: "+info.response)
-    })
+  var mailOptions = {
+    from: 'iconsrequestservice@gmail.com',
+    to: recipient,
+    subject: 'Sending Email using Node.js',
+    text: `Hi Smartherd, thank you for your nice Node.js tutorials.
+            I will donate 50$ for this course. Please send me payment options.`
+    // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+  };
 
-    console.log(false)
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('success');
+    }
+  });
 
-return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
-    },
-    body: "success"
-  }}
-  catch (err) {
-    return {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
-        },
-        body: JSON.stringify(err)
-      }
+
+}
+
+function valid(email){
+  if (email.match(/.+?@.+?\..+?/g) != null){
+    return email;
   }
+  return null
 }
