ddcd3dd46b62691d851967dd0e891e0403bcf173
Jacob Tepperman <jtepp@icloud.com>
2021-03-27 00:53:44 -0400

    added new function card and 2 card

--- dist/functions.js
+++ dist/functions.js
@@ -29,13 +29,29 @@ refreshTooltips()
 document.onscroll = () => refreshTooltips()
 document.onresize = () => refreshTooltips()
 
-document.getElementById('missionstest').onclick = async ()=>{
+document.getElementById('missionstestbutton').onclick = async ()=>{
+    return await fetch("https://allpurpose.netlify.app/.netlify/functions/missions")
+    .then(res => res.json())
+    .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
+}
+document.getElementById('missionstesttitle').onclick = async ()=>{
     return await fetch("https://allpurpose.netlify.app/.netlify/functions/missions")
     .then(res => res.json())
     .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
 }
 
-document.getElementById('coordstest').onclick = async ()=>{
+document.getElementById('coordstestbutton').onclick = async ()=>{
+    if (coordSearch.value != '') {
+    return await fetch("https://allpurpose.netlify.app/.netlify/functions/coordinates?a="+coordSearch.value)
+    .then(res => res.text())
+    .then(data => 
+             document.getElementById('coordstext').innerText = data
+        )
+        .catch(err=> document.getElementById('coordstext').innerText = "Please enter a valid location name")
+}
+else document.getElementById('coordstext').innerText = "Please enter a valid location name"
+}
+document.getElementById('coordstesttitle').onclick = async ()=>{
     if (coordSearch.value != '') {
     return await fetch("https://allpurpose.netlify.app/.netlify/functions/coordinates?a="+coordSearch.value)
     .then(res => res.text())
@@ -47,54 +63,54 @@ document.getElementById('coordstest').onclick = async ()=>{
 else document.getElementById('coordstext').innerText = "Please enter a valid location name"
 }
 
-document.getElementById('imgtest').onclick = async ()=>{
-    if (imgSearch.value != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/images?q="+imgSearch.value+"&b64")
-    .then(res => res.text())
-    .then(data => 
-             document.getElementById('imgimg').src = "data:image/jpeg;charset=utf-8;base64,"+data
-        )
-        .catch( err => document.getElementById('imgimg').src = "")
+// document.getElementById('imgtest').onclick = async ()=>{
+//     if (imgSearch.value != '') {
+//     return await fetch("https://allpurpose.netlify.app/.netlify/functions/images?q="+imgSearch.value+"&b64")
+//     .then(res => res.text())
+//     .then(data => 
+//              document.getElementById('imgimg').src = "data:image/jpeg;charset=utf-8;base64,"+data
+//         )
+//         .catch( err => document.getElementById('imgimg').src = "")
 
-}
-else document.getElementById('imgimg').src = ""
-}
-
-
-document.getElementById('datetest').onclick = async ()=>{
-    if (dateSearch.value != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/dateParse?a="+dateSearch.value)
-    .then(res => res.text())
-    .then(data => 
-             document.getElementById('datetext').innerText = data
-        )
-        .catch(err=> document.getElementById('datetext').innerText = "Invalid Date")
-}
-else document.getElementById('datetext').innerText = "Invalid Date"
-}
+// }
+// else document.getElementById('imgimg').src = ""
+// }
 
 
+// document.getElementById('datetest').onclick = async ()=>{
+//     if (dateSearch.value != '') {
+//     return await fetch("https://allpurpose.netlify.app/.netlify/functions/dateParse?a="+dateSearch.value)
+//     .then(res => res.text())
+//     .then(data => 
+//              document.getElementById('datetext').innerText = data
+//         )
+//         .catch(err=> document.getElementById('datetext').innerText = "Invalid Date")
+// }
+// else document.getElementById('datetext').innerText = "Invalid Date"
+// }
 
 
 
-document.getElementById('redtest').onclick = async ()=>{
-    if (redSearch.value != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/reddit?sort=top&limit=1&sub="+redSearch.value)
-    .then(res => res.text())
-    .then(data => 
-             {
-                if (data == "[]") document.getElementById('redtext').innerText = "Invalid Subreddit"
-                else document.getElementById('redtext').innerText = JSON.stringify(JSON.parse(data)[0])
-            }
-        )
-        .catch(err=> document.getElementById('redtext').innerText = "Invalid Subreddit")
-}
-else document.getElementById('redtext').innerText = "Invalid Subreddit"
-}
+
+
+// document.getElementById('redtest').onclick = async ()=>{
+//     if (redSearch.value != '') {
+//     return await fetch("https://allpurpose.netlify.app/.netlify/functions/reddit?sort=top&limit=1&sub="+redSearch.value)
+//     .then(res => res.text())
+//     .then(data => 
+//              {
+//                 if (data == "[]") document.getElementById('redtext').innerText = "Invalid Subreddit"
+//                 else document.getElementById('redtext').innerText = JSON.stringify(JSON.parse(data)[0])
+//             }
+//         )
+//         .catch(err=> document.getElementById('redtext').innerText = "Invalid Subreddit")
+// }
+// else document.getElementById('redtext').innerText = "Invalid Subreddit"
+// }
 
 
 for (let f of document.getElementsByTagName("form")){
-    f.onsubmit = (e)=>{e.preventDefault(); f.children[0].children[2].click()}
+    f.onsubmit = (e)=>{e.preventDefault(); f.children[0].children[0].click()}
     
 }
 
