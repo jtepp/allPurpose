8cee8672a17ff4e7a0a3af1b96c9b97bdf839c59
Jacob Tepperman <jtepp@icloud.com>
2021-07-20 18:17:32 -0400

    added bounce

--- dist/alexander/test.js
+++ dist/alexander/test.js
@@ -7,7 +7,7 @@ prep(b1, "shake-animation", 200)
 
 prep(b2, "red")
 
-prep(b3, "unfilled-box")
+// prep(b3, "unfilled-box")
 
 
 function prep(el, an, timeout) {
@@ -21,8 +21,20 @@ function prep(el, an, timeout) {
     }
 }
 
+b3.onclick = () => {
+    if (box.classList.contains("unfilled-box")) {
+        box.classList.add("bounce")
+        setTimeout(() => {
+            box.classList.remove("bounce")
+        }, 100)
+    }
+    box.classList.add("unfilled-box")
+}
+
+
 document.body.oninput = () => {
     box.classList.remove("shake-animation")
     box.classList.remove("red")
     box.classList.remove("unfilled-box")
+    box.classList.remove("bounce")
 }
