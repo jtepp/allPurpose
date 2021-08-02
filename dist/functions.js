127801e023da2db9bc610c5c33eb156ded50ad89
Jacob Tepperman <jtepp@icloud.com>
2021-08-02 15:44:15 -0400

    added picker stuff

--- dist/functions.js
+++ dist/functions.js
@@ -9,52 +9,51 @@ const first = document.getElementById('first')
 const last = document.getElementById('last')
 var i = 0
 
-for (let l of "Jacob"){
+for (let l of "Jacob") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     first.appendChild(e)
     i++
 }
-for (let l of "Tepperman"){
+for (let l of "Tepperman") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     last.appendChild(e)
     i++
 }
 
 //missions
-document.getElementById('missionstestbutton').onclick = async ()=>{
+document.getElementById('missionstestbutton').onclick = async () => {
     return await fetch("https://allpurpose.netlify.app/.netlify/functions/missions")
-    .then(res => res.json())
-    .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
+        .then(res => res.json())
+        .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
 }
 
-document.getElementById('missionstesttitle').onclick = async ()=>{
+document.getElementById('missionstesttitle').onclick = async () => {
     return await fetch("https://allpurpose.netlify.app/.netlify/functions/missions")
-    .then(res => res.json())
-    .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
+        .then(res => res.json())
+        .then(data => document.getElementById('missionstext').innerText = JSON.stringify(data))
 }
 
 //coords
-document.getElementById('coordstestbutton').onclick = async ()=>{
+document.getElementById('coordstestbutton').onclick = async () => {
     if (coordSearch.value != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/coordinates?a="+coordSearch.innerText)
-    .then(res => res.text())
-    .then(data => 
-             document.getElementById('coordstext').innerText = data
-        )
-        .catch(err=> document.getElementById('coordstext').innerText = "Please enter a valid location name")
-}
-else document.getElementById('coordstext').innerText = "Please enter a valid location name"
+        return await fetch("https://allpurpose.netlify.app/.netlify/functions/coordinates?a=" + coordSearch.innerText)
+            .then(res => res.text())
+            .then(data =>
+                document.getElementById('coordstext').innerText = data
+            )
+            .catch(err => document.getElementById('coordstext').innerText = "Please enter a valid location name")
+    } else document.getElementById('coordstext').innerText = "Please enter a valid location name"
 }
 
-document.getElementById('coordstesttitle').onclick = ()=>{
+document.getElementById('coordstesttitle').onclick = () => {
     document.getElementById('coordstestbutton').click()
 }
 
-coordSearch.onkeydown = (e)=>{
+coordSearch.onkeydown = (e) => {
     if (e.keyCode == 13) {
         e.preventDefault()
         document.getElementById('coordstestbutton').click()
@@ -62,24 +61,23 @@ coordSearch.onkeydown = (e)=>{
 }
 
 //img
-document.getElementById('imgtestbutton').onclick = async ()=>{
+document.getElementById('imgtestbutton').onclick = async () => {
     if (imgSearch.innerText != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/images?q="+imgSearch.innerText+"&b64")
-    .then(res => res.text())
-    .then(data => 
-             document.getElementById('imgimg').src = "data:image/jpeg;charset=utf-8;base64,"+data
-        )
-        .catch( err => document.getElementById('imgimg').src = "")
+        return await fetch("https://allpurpose.netlify.app/.netlify/functions/images?q=" + imgSearch.innerText + "&b64")
+            .then(res => res.text())
+            .then(data =>
+                document.getElementById('imgimg').src = "data:image/jpeg;charset=utf-8;base64," + data
+            )
+            .catch(err => document.getElementById('imgimg').src = "")
 
-}
-else document.getElementById('imgimg').src = ""
+    } else document.getElementById('imgimg').src = ""
 }
 
-document.getElementById('imgtesttitle').onclick = ()=>{
+document.getElementById('imgtesttitle').onclick = () => {
     document.getElementById('imgtestbutton').click()
 }
 
-imgSearch.onkeydown = (e)=>{
+imgSearch.onkeydown = (e) => {
     if (e.keyCode == 13) {
         e.preventDefault()
         document.getElementById('imgtestbutton').click()
@@ -87,23 +85,22 @@ imgSearch.onkeydown = (e)=>{
 }
 
 //date
-document.getElementById('datetestbutton').onclick = async ()=>{
+document.getElementById('datetestbutton').onclick = async () => {
     if (dateSearch.innerText != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/dateParse?a="+dateSearch.innerText)
-    .then(res => res.text())
-    .then(data => 
-             document.getElementById('datetext').innerText = data
-        )
-        .catch(err=> document.getElementById('datetext').innerText = "Invalid Date")
-}
-else document.getElementById('datetext').innerText = "Invalid Date"
+        return await fetch("https://allpurpose.netlify.app/.netlify/functions/dateParse?a=" + dateSearch.innerText)
+            .then(res => res.text())
+            .then(data =>
+                document.getElementById('datetext').innerText = data
+            )
+            .catch(err => document.getElementById('datetext').innerText = "Invalid Date")
+    } else document.getElementById('datetext').innerText = "Invalid Date"
 }
 
-document.getElementById('datetesttitle').onclick = ()=>{
+document.getElementById('datetesttitle').onclick = () => {
     document.getElementById('datetestbutton').click()
 }
 
-dateSearch.onkeydown = (e)=>{
+dateSearch.onkeydown = (e) => {
     if (e.keyCode == 13) {
         e.preventDefault()
         document.getElementById('datetestbutton').click()
@@ -111,29 +108,25 @@ dateSearch.onkeydown = (e)=>{
 }
 
 //red
-document.getElementById('redtestbutton').onclick = async ()=>{
+document.getElementById('redtestbutton').onclick = async () => {
     if (redSearch.innerText != '') {
-    return await fetch("https://allpurpose.netlify.app/.netlify/functions/reddit?sort=top&limit=1&sub="+redSearch.innerText)
-    .then(res => res.text())
-    .then(data => 
-             {
+        return await fetch("https://allpurpose.netlify.app/.netlify/functions/reddit?sort=top&limit=1&sub=" + redSearch.innerText)
+            .then(res => res.text())
+            .then(data => {
                 if (data == "[]") document.getElementById('redtext').innerText = "Invalid Subreddit"
                 else document.getElementById('redtext').innerText = JSON.stringify(JSON.parse(data)[1])
-            }
-        )
-        .catch(err=> document.getElementById('redtext').innerText = "Invalid Subreddit")
-}
-else document.getElementById('redtext').innerText = "Invalid Subreddit"
+            })
+            .catch(err => document.getElementById('redtext').innerText = "Invalid Subreddit")
+    } else document.getElementById('redtext').innerText = "Invalid Subreddit"
 }
 
-document.getElementById('redtesttitle').onclick = ()=>{
+document.getElementById('redtesttitle').onclick = () => {
     document.getElementById('redtestbutton').click()
 }
 
-redSearch.onkeydown = (e)=>{
+redSearch.onkeydown = (e) => {
     if (e.keyCode == 13) {
         e.preventDefault()
         document.getElementById('redtestbutton').click()
     }
 }
-
--- dist/projects.js
+++ dist/projects.js
@@ -3,17 +3,38 @@ const first = document.getElementById('first')
 const last = document.getElementById('last')
 var i = 0
 
-for (let l of "Jacob"){
+for (let l of "Jacob") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     first.appendChild(e)
     i++
 }
-for (let l of "Tepperman"){
+for (let l of "Tepperman") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     last.appendChild(e)
     i++
+}
+
+//picker
+for (let el of document.getElementsByClassName("picker-item")) {
+    el.onclick = () => {
+        movePickerOver(el)
+    }
+}
+
+function movePickerOver(element) {
+    document.documentElement.style.setProperty("--over-left-" + element.parentNode.getAttribute("tag"), `${indexOf(element.parentNode.children, element) * 100}px`);
+    element.parentNode.setAttribute("value", element.getAttribute("value"))
+}
+
+function indexOf(collection, element) {
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] == element) {
+            return i
+        }
+    }
+    return -1
 }
