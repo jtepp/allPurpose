dcc968e96049e6d72f32ba115b76d150c737742a
Jacob Tepperman <jtepp@icloud.com>
2021-08-13 07:53:55 -0400

    added changepage

--- dist/index.js
+++ dist/index.js
@@ -4,17 +4,27 @@ const first = document.getElementById('first')
 const last = document.getElementById('last')
 var i = 0
 
-for (let l of "Jacob"){
+for (let l of "Jacob") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     first.appendChild(e)
     i++
 }
-for (let l of "Tepperman"){
+for (let l of "Tepperman") {
     const e = document.createElement("x")
     e.innerHTML = l
-    e.style.animationDelay = i-len+"s"
+    e.style.animationDelay = i - len + "s"
     last.appendChild(e)
     i++
+}
+
+// function to change page without re-rendering
+function changePage(page) {
+    fetch("https://jacobtepperman.com/page/" + page)
+        .then(response => response.text())
+        .then(text => {
+            console.log(text)
+            window.history.pushState("/" + page, page + " | Jacob Tepperman", "/" + page)
+        })
 }
