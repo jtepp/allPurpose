1fcb8ddc308df8c34666435431a2732b561d51e3
Jacob Tepperman <jtepp@icloud.com>
2021-08-13 19:43:38 -0400

    made picker responsive

--- dist/projects.js
+++ dist/projects.js
@@ -1,13 +1,29 @@
-//picker
+let lastIndex = {
+    "purpose": 0,
+    "platform": 0,
+}
+
+
 for (let el of document.getElementsByClassName("picker-item")) {
     el.onclick = () => {
         movePickerOver(el)
     }
 }
 
+document.body.onresize = () => {
+    // document.querySelector(":root").style.setProperty("--picker-item-width"
+    document.documentElement.style.setProperty("--over-left-purpose",
+        `${lastIndex["purpose"] * getWidth()}px`);
+
+    document.documentElement.style.setProperty("--over-left-platform",
+        `${lastIndex["platform"] * getWidth()}px`);
+}
+
 function movePickerOver(element) {
-    document.documentElement.style.setProperty("--over-left-" + element.parentNode.getAttribute("tag"), `${indexOf(element.parentNode.children, element) * 100}px`);
+    document.documentElement.style.setProperty("--over-left-" + element.parentNode.getAttribute("tag"),
+        `${indexOf(element.parentNode.children, element) * getWidth()}px`);
     element.parentNode.setAttribute("value", element.getAttribute("value"))
+    lastIndex[element.parentNode.getAttribute("tag")] = indexOf(element.parentNode.children, element)
 }
 
 function indexOf(collection, element) {
@@ -17,4 +33,8 @@ function indexOf(collection, element) {
         }
     }
     return -1
+}
+
+function getWidth() {
+    return document.querySelector(".picker-item").getBoundingClientRect().width
 }
