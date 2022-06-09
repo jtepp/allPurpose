a375e89d7c2a8ffc62eec7daa0eaf4f6e746f7f0
Jacob Tepperman <jtepp@icloud.com>
2022-06-09 19:18:48 -0400

    finished amazon endpoint

--- src/amazon.js
+++ src/amazon.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch"
+const matchAll = require('string.prototype.matchall');
 
-
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
     return fetch("https://www.amazon.com/s?k=" + event.queryStringParameters["q"])
         .then(res => res.text())
         .then(html => {
-            const document = new DOMParser().parseFromString(html, "text/html");
-            const results = Array(...document.querySelectorAll(".s-image")).map(el => el.src);
+            let images = {
+                images: [...matchAll(JSON.stringify(html), /\\\"s-image\\\".+?src=\\\"(.+?)\\\".+?srcset/g)].map(x => x[1])
+            }
 
             return ({
                 statusCode: 200,
@@ -14,9 +15,7 @@ exports.handler = async (event, context) => {
                     "Access-Control-Allow-Origin": "*", // Required for CORS support to work
                     "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
                 },
-                body: {
-                    images: results
-                }
+                body: JSON.stringify(images)
             })
         })
         .catch(error => ({
