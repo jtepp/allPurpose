d88d1610a1681334c29f0bb139bf44b78b55ccc7
Jacob Tepperman <jtepp@icloud.com>
2022-06-09 17:09:37 -0400

    return images

--- src/amazon.js
+++ src/amazon.js
@@ -5,13 +5,18 @@ exports.handler = async (event, context) => {
     return fetch("https://www.amazon.com/s?k=" + event.queryStringParameters["q"])
         .then(res => res.text())
         .then(html => {
+            const document = new DOMParser().parseFromString(html, "text/html");
+            const results = Array(...document.querySelectorAll(".s-image")).map(el => el.src);
+
             return ({
                 statusCode: 200,
                 headers: {
                     "Access-Control-Allow-Origin": "*", // Required for CORS support to work
                     "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS 
                 },
-                body: html
+                body: {
+                    images: results
+                }
             })
         })
         .catch(error => ({
