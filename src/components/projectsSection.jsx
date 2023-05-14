4a35a635aa73eb1da3392713e9853cfd52c61edd
Jacob Tepperman <jtepp@icloud.com>
2023-05-14 18:02:10 -0400

    added first project prototype

--- src/components/projectsSection.jsx
+++ src/components/projectsSection.jsx
@@ -6,6 +6,15 @@ function ProjectsSection() {
     return ( 
         <Section id='projects'>
                 <h1 id='projects-title'>Projects</h1>
+
+                <div id='projects-content'>
+                    <div id="projects-container">
+                        <div className="project">
+                            <h2>Project 1</h2>
+                            <p>Project 1 description</p>
+                        </div>
+                    </div>
+                </div>
         </Section>
      );
 }
