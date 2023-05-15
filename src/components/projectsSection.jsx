1f1a913e27db3e43bdd15a800d3d69e5fa439bfd
Jacob Tepperman <jtepp@icloud.com>
2023-05-14 23:52:38 -0400

    fixed 1 rotation, added shadow too. added pictures & test projects

--- src/components/projectsSection.jsx
+++ src/components/projectsSection.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Section from './section';
 import '../css/projects.css'
 import Project from './project';
+const data = require('../res/projects/projectsData.json')
 
 function ProjectsSection() {
+    const projects = data.map((project, index) => {
+        return <Project key={index} id={index} title={project.title}
+        description={project.description} platform={project.platform} />
+    })
     return ( 
         <Section id='projects'>
                 <h1 id='projects-title'>Projects</h1>
 
                 <div id='projects-content'>
                     <div id="projects-container">
-                        <Project id='test' />
+                        {projects}
                     </div>
                 </div>
         </Section>
