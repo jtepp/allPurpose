f9df933fcdea883649bd766e6ec3931a5e2c2ac8
Jacob Tepperman <jtepp@icloud.com>
2023-05-26 18:32:15 -0400

    created GameCase and crt

--- src/components/minigames.jsx
+++ src/components/minigames.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { GameCase } from './minigames/gameCase'
 
 function Minigames(props) {
     return ( 
         <div id="minigames">
             <h1>MINIGAMESFI:JNES:FKNDKLDJF</h1>
+            <GameCase />
         </div>
      );
 }
---
+++ src/components/minigames/crt.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../css/crt.css'
+
+function CRT() {
+    return ( 
+        <div id='crt'>
+            <img src="" alt="" />
+        </div>
+     );
+}
+
+export default CRT;
