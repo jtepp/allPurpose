7f2336930871e8331823e1b8ea85977ed05feb07
Jacob Tepperman <jtepp@icloud.com>
2023-05-26 21:22:51 -0400

    finished CRT display

--- src/components/minigames/crt.jsx
+++ src/components/minigames/crt.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import '../../css/crt.css'
 import CRTborder from '../../res/minigames/crt-border.png'
-import staticGIF from '../../res/minigames/static.gif'
 
-export default function CRT() {
+export default function CRT(props) {
     return ( 
         <div id='crt-container'>
-            <img id='crt-img' src={CRTborder} alt="" />
-            <img id='static-gif' src={staticGIF} alt="" />
+            <div id='crt-static' className='crt-internal'></div>
+            <div id="crt-content" className='crt-internal'>
+                {props.children}
+            </div>
+            <div id="crt-shadow" className='crt-internal'></div>
+            <div id="crt-screen-door-rows" className='crt-internal'></div>
+            <div id="crt-screen-door-cols" className='crt-internal'></div>
+            <img id='crt-border' src={CRTborder} alt="" />
         </div>
      );
 }
--- src/components/minigames/minigames.jsx
+++ src/components/minigames/minigames.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { GameCase } from './gameCase'
+// import { GameCase } from './gameCase'
 import "../../css/minigames.css"
 import Section from '../section';
 import CRT from './crt';
+import { thumbnails } from '../home';
 
 function Minigames(props) {
     return ( 
         <Section id="minigames">
             <CRT>
-                
+                <img src={thumbnails["larrybirdle"]} alt="" />
             </CRT>
         </Section>
      );
