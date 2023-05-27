e382a451d5539c6c47ecb34130b1581df761d954
Jacob Tepperman <jtepp@icloud.com>
2023-05-26 21:59:01 -0400

    max crt width

--- src/components/minigames/minigames.jsx
+++ src/components/minigames/minigames.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 // import { GameCase } from './gameCase'
 import "../../css/minigames.css"
 import Section from '../section';
-import CRT from './crt';
+import CRT from './CRT';
 import { thumbnails } from '../home';
 
 function Minigames(props) {
     return ( 
         <Section id="minigames">
-            <CRT>
-                <img src={thumbnails["larrybirdle"]} alt="" />
-            </CRT>
+            <div id="minigames-content">
+                <CRT>
+                    <img src={thumbnails["larrybirdle"]} alt="" />
+                </CRT>
+            </div>
         </Section>
      );
 }
