5995d28ee706594378ccb4d9121061762c78b993
Jacob Tepperman <jtepp@icloud.com>
2023-05-27 16:16:14 -0400

    added text stroke

--- src/components/minigames/crt.jsx
+++ src/components/minigames/crt.jsx
@@ -9,9 +9,7 @@ export default function CRT(props) {
             <div id="crt-content" className='crt-internal'>
                 {props.children}
             </div>
-            <div className="crt-text crt-internal">
-                Input 3
-            </div>
+            <div className="crt-text crt-internal">Input 3</div>
             <div id="crt-shadow" className='crt-internal'></div>
             <div id="crt-screen-door-rows" className='crt-internal'></div>
             <div id="crt-screen-door-cols" className='crt-internal'></div>
