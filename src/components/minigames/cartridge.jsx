bd15728e8cc684109e85b84ac45cc4fa6cd2fa76
Jacob Tepperman <jtepp@icloud.com>
2023-05-28 00:43:45 -0400

    set up empty tv start

--- src/components/minigames/cartridge.jsx
+++ src/components/minigames/cartridge.jsx
@@ -3,7 +3,9 @@ import '../../css/cartridge.css'
 
 function Cartridge(props) {
     return (
-    <div class="game-cartridge-cont">
+    <div onClick={props.onClick} index={props.index} style={{
+        order: props.index,
+    }} class={props.className + " game-cartridge-cont"}>
             <div class="game-cartridge" title={props.title}>
                 <img src={props.img} alt="" class="cartridge-img" />
             <div class="game-cartridge-overlay"></div>
--- src/components/minigames/minigames.jsx
+++ src/components/minigames/minigames.jsx
@@ -10,14 +10,14 @@ import Cartridge from './cartridge';
 const gameThumbs = importAll(require.context('../../res/minigames/thumbs', false, /\.(png|jpe?g|svg|gif)$/))
 
 function Minigames(props) {
-    const [currentGameIndex, setCurrentGameIndex] = useState(0)
+    const [currentGameIndex, setCurrentGameIndex] = useState(-1)
 
     const resizeIframe = () => {
         const iframe = document.querySelector('#game-iframe')
         const cont = document.querySelector('#crt-content')
         // const calcPad = getComputedStyle(cont).padding.replace('px', '')
-
-        const padFactor = (gameData[currentGameIndex].height === 600 ? 0.18 : gameData[currentGameIndex].width === 600 ? 0.12 : 0.15)
+        const i = currentGameIndex === -1 ? 0 : currentGameIndex
+        const padFactor = (gameData[i].height === 600 ? 0.18 : gameData[i].width === 600 ? 0.12 : 0.15)
         const padding = 2*(padFactor*cont.offsetWidth) + 80
         // console.log(padding)
         // scale the iframe down so that the longest dimension fits in the container
@@ -32,17 +32,32 @@ function Minigames(props) {
         document.querySelector(':root').style.setProperty('--crt-width', `${cont.offsetWidth}px`)
     }
 
+    // useEffect(()=> {
+    //     changeGame(0)
+    // }
     useEffect(()=> {
         document.querySelector('#game-iframe').onload = resizeIframe
         resizeIframe()
     }, [props.resizeState, currentGameIndex, resizeIframe])
 
     const changeGame = (index) => {
+        const current = document.querySelector('.current-game')
+        const next = document.querySelector("[index='" + index + "']")
+        let order = 0
+        if (current) {
+            order = Number(current.getAttribute('index'))
+            current.classList.remove('current-game')
+        }
+        next.classList.add('current-game')
+        next.style.order = order
+
         setCurrentGameIndex(index)
     }
 
     const cartridges = gameData.map((game, index) => {
-        return <Cartridge img={gameThumbs[game.img]} title={game.short || game.title}/>
+        return <Cartridge index={index} onClick={()=>{
+            changeGame(index)
+        }} img={gameThumbs[game.img]} title={game.short || game.title} />
     })
 
     return ( 
@@ -51,26 +66,26 @@ function Minigames(props) {
                 <div id="game-container">
                     <div id="game-info">
                         <Cutout id="minigame-cutout">
-                            <h1 id="game-title">{gameData[currentGameIndex].title}</h1>
+                            <h1 id="game-title">{currentGameIndex === -1 ? "Minigames" : gameData[currentGameIndex].title}</h1>
                         </Cutout>
-                            <h3 id="game-description">{gameData[currentGameIndex].description}</h3>
+                            <h3 id="game-description">{currentGameIndex === -1 ? "Click on a game cartdrige below to play!" : gameData[currentGameIndex].description}</h3>
                             <br />
-                            <h2 id='game-controls-heading'>Controls</h2>
-                            <h3 id="game-controls">{ReactHTMLParser(gameData[currentGameIndex].controls)}</h3>
+                            <h2 id='game-controls-heading'>{currentGameIndex === -1 ? "" : "Controls"}</h2>
+                            <h3 id="game-controls">{ReactHTMLParser(currentGameIndex === -1 ? "" : gameData[currentGameIndex].controls)}</h3>
                         </div>
                         <CRT currentGameIndex={currentGameIndex} resizeState={props.resizeState}
-                        bgColor={gameData[currentGameIndex].bgColor} text={gameData[currentGameIndex].title}
+                        bgColor={currentGameIndex === -1 ? "transparent" : gameData[currentGameIndex].bgColor} text={currentGameIndex === -1 ? "Input 3" : gameData[currentGameIndex].title}
                         onClick={()=>{
                             changeGame((currentGameIndex + 1) % gameData.length)
                         }}>
                             <iframe id='game-iframe' title='game'
-                            src={`https://www.khanacademy.org/computer-programming${gameData[currentGameIndex].url}embedded?editor=no&buttons=no&author=no&embed=yes`}
-                            width={gameData[currentGameIndex].width}
-                            height={gameData[currentGameIndex].height}                    
+                            src={currentGameIndex === -1 ? "" : `https://www.khanacademy.org/computer-programming${gameData[currentGameIndex].url}embedded?editor=no&buttons=no&author=no&embed=yes`}
+                            width={currentGameIndex === -1 ? 0 : gameData[currentGameIndex].width}
+                            height={currentGameIndex === -1 ? 0 : gameData[currentGameIndex].height}                    
                             frameborder="0" scrolling="no" border="0"
                             style={{
-                                minWidth: gameData[currentGameIndex].width,
-                                minHeight: gameData[currentGameIndex].height,
+                                minWidth: currentGameIndex === -1 ? "100%" : gameData[currentGameIndex].width,
+                                minHeight: currentGameIndex === -1 ? "100%" : gameData[currentGameIndex].height,
                             }}
                             />
                     </CRT>
